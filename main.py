4d45e67e5b4f94d37247591eb6a01c84b32f612d
srub <shani.rubovitch@gmail.com>
2020-05-23 17:39:53 +0200

    now with rects instead of dots.

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
 import pyb
-
+from random import randint
 import lcd160cr
 lcd = lcd160cr.LCD160CR('X')
 accel = pyb.Accel()
 
-from random import randint
+SENSITIVITY = 2                         # minimal sensitivity for the accel
+WIDTH = 4                               # food and moving rect width and height
 
-SENSITIVITY = 3
+bg = lcd.rgb(255, 255, 255)             # background color = white
+rect_fill_color = lcd.rgb(255, 0, 127)  # moving rect fill color = pink
+rect_line_color = lcd.rgb(255, 0, 0)    # moving rect line color
 
-bg = lcd.rgb(255, 255, 255)
+rect_x = lcd.w / 2                      # initial x value for rect
+rect_y = lcd.h / 2                      # initial y value for rect
 
-dot_x = lcd.w / 2
-dot_y = lcd.h / 2
-dot_color = lcd.rgb(0, 0, 0)
+food_x = randint(0, lcd.w - WIDTH)          # initial x value for first food
+food_y = randint(0, lcd.h - WIDTH)          # initial y value for first food
+if food_x % 2 != 0:
+    food_x += 1
+if food_y % 2 != 0:
+    food_y += 1
 
-food_x = randint(0, lcd.w)
-food_y = randint(0, lcd.h)
-food_color = lcd.rgb(255, 0 ,127)
-draw_food = True
+food_color = lcd.rgb(0, 0 ,0)           # food color = black (to be used for both line and fill colors)
+
+draw_food = True                        # do we need to draw the food?
 
 while True:
-    lcd.erase()
-    lcd.set_pen(dot_color, bg)   
-    lcd.dot(int(dot_x), int(dot_y))
-    x = accel.x()
+    lcd.set_pen(rect_line_color, bg)   
+    lcd.erase()                                         # backgroud is set to white
+    lcd.set_pen(rect_line_color, rect_fill_color)
+    lcd.rect(int(rect_x), int(rect_y), WIDTH, WIDTH)    # rect is drawn with its colots and x,y values
+
+    x = accel.x()                                       # accel objects
     y = accel.y()
 
-    if abs(x) > SENSITIVITY:
-        dot_x += x
+    if abs(x) > SENSITIVITY:                            # identifing movement in x axis
+        rect_x += x             
 
-    if abs(y) > SENSITIVITY:
-        dot_y -= y
+    if abs(y) > SENSITIVITY:                            # identifing movement in y axis
+        rect_y -= y
 
-    if draw_food:
-        lcd.set_pen(food_color, bg)
-        lcd.dot(food_x, food_y)
+    if draw_food:                                       # drawing the food
+        lcd.set_pen(food_color, food_color)
+        lcd.rect(int(food_x), int(food_y), WIDTH, WIDTH)
 
-    if (dot_x == food_x and dot_y == food_y):
+    if (rect_x == food_x and rect_y == food_y):
         lcd.write("very good")
         break
 
